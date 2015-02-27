132067c491fb62fd8148e246d19cf1d12ea42af3
gbear47 <agabourie@wisc.edu>
2015-02-27 17:36:39 -0600

    added some more initial parameters

--- MATLAB/Sample_input_signal.m
+++ MATLAB/Sample_input_signal.m
@@ -11,10 +11,51 @@
 
 %% Input Parameters
 %Frequency
-f = 150e6; 
+f = 150e6; %[hz]
+
+%E-field magnitude
+E_o = 1; %[V/m]
+
+%Vacuum permittivity 
+epsilon_o = 8.8541878176e-12; %[F/m]
+epsilon_rel = 1;
+
+%permeability
+mu_o = (4*pi)*10^(-7); %[H/m]
+mu_rel = 1;
+%recall that [F/m]*[H/m] = s^2/m^2 = 1/c
+
+%The four antennas will be placed in a square grid like:
+%  4    3
+%
+%  1    2
+%
+% The separation constant (like lattice constant) is 
+a = .5; %[m]
+
+%From this the positions of the antennas can be determined
+r1 = [0, 0];
+r2 = [a, 0];
+r3 = [a, a];
+r4 = [0, a];
+
+%Propagation direction (theta = 0 => dirction is from antenna 1 to 2)
+theta = 60; %[degrees]
 
 
 %% Calculations
 
 %angular frequency based off frequency
 omega = 2*pi*f;
+
+%permitivity & permeability (same units)
+eps = epsilon_o*epsilon_rel;
+mu = mu_o*mu_rel;
+
+%propagation constant
+beta = omega*sqrt(mu*eps); %[m^-1]
+
+% The signal that we will be looking at is a 2D E-field of an EM wave
+% propagating with no phase shift between H & E.
+
+
