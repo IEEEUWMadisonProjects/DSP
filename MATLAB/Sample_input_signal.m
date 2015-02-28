ff130edd0230fe6ac1a54ef0bfe040e8c6976226
gbear47 <agabourie@wisc.edu>
2015-02-27 19:17:59 -0600

    Finished plotting real parts of waves recorded at different positions

--- MATLAB/Sample_input_signal.m
+++ MATLAB/Sample_input_signal.m
@@ -9,12 +9,17 @@
 % understand how to interpret phase changes to find the direction of the
 % wave propagation.
 
+%% Initialize Workspace
+clear;
+close all;
+clc;
+
 %% Input Parameters
 %Frequency
 f = 150e6; %[hz]
 
 %E-field magnitude
-E_o = 1; %[V/m]
+E_o = 1; %[V/m] Not used as of now
 
 %Vacuum permittivity 
 epsilon_o = 8.8541878176e-12; %[F/m]
@@ -31,7 +36,7 @@ mu_rel = 1;
 %  1    2
 %
 % The separation constant (like lattice constant) is 
-a = .5; %[m]
+a = 1; %[m]
 
 %From this the positions of the antennas can be determined
 r1 = [0, 0];
@@ -39,9 +44,12 @@ r2 = [a, 0];
 r3 = [a, a];
 r4 = [0, a];
 
-%Propagation direction (theta = 0 => dirction is from antenna 1 to 2)
-theta = 60; %[degrees]
+%For plotting
+r_all = [r1;r2;r3;r4];
 
+%Propagation direction (theta = 0 => dirction is from antenna 1 to 2 and 
+%   increasing theta moves counter-clockwise)
+theta = 2*pi*60/360; %[radians]
 
 %% Calculations
 
@@ -55,7 +63,57 @@ mu = mu_o*mu_rel;
 %propagation constant
 beta = omega*sqrt(mu*eps); %[m^-1]
 
-% The signal that we will be looking at is a 2D E-field of an EM wave
-% propagating with no phase shift between H & E.
+%Propagation direction vector
+k = [1,0];
+rot = [cos(theta),-sin(theta);sin(theta),cos(theta)];
+k = (rot*k')';
+
+%prop constant dotted with r as in time-harmonic exponential. We are only
+% looking at a few positions, so these can be pre-calculated
+bkr = beta*[dot(k,r1);
+            dot(k,r2);
+            dot(k,r3);
+            dot(k,r4)];
+
+%want 5 periods of information collected
+T = 1/f;
+t = linspace(0,5*T, 1000); %time vector based on frequency
+
+%Electric field matrix initialized to num time steps & antennas
+E = zeros(length(bkr),length(t));
+
+for m=1:length(t)
+    for n=1:length(bkr)
+        E(n,m) = exp(1i*(omega*t(m)-bkr(n)));
+    end
+end
 
 
+numPlots = length(bkr);
+gridSize = numPlots;
+if sqrt(numPlots)==floor(sqrt(numPlots))
+    gridSize = sqrt(numPlots);
+else
+    gridSize = floor(sqrt(numPlots))+1;
+end
+    
+    
+figure;
+for m=1:length(bkr)
+    subplot(gridSize,gridSize,m);
+    plot(t,real(E(m,:)));
+    title(['Antenna ' num2str(m)]);
+    xlabel('Time [S]');
+    ylabel('Amplitude [V/m]');
+    axis([0 t(end) -E_o E_o]);
+end
+
+
+% figure;
+% quiver(0,0,k(1),k(2));
+% hold on;
+% scatter(r_all(:,1), r_all(:,2));
+% axis([-.05 1.05 -.05 1.05]);
+% title('Initial Setup');
+% xlabel('x');
+% ylabel('y');
